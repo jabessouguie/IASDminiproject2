2c54d2a6ade42322dfec25b771c27c7f8ffb00dd
jabessouguie <jeansebastienab@gmail.com>
2021-01-03 16:34:03 +0000

    modify test.py

--- solution.py
+++ solution.py
@@ -1,13 +1,17 @@
 import probability
 
 class MDProblem :
-def __init__ (self , fh):
-# Place here your code to load problem from opened file object fh
-# and use probability . BayesNet () to create the Bayesian network .
+    def __init__ (self , fh):
+        """
+                Initialize the network; read and parse the given file.
+                Args:
+                    fh:  Name of the file containing the data.
+                """
+        return ()
 
-def solve ( self ):
-# Place here your code to determine the maximum likelihood
-# solution returning the solution disease name and likelihood .
-# Use probability . elimination_ask () to perform probabilistic
-# inference .
-return ( disease , likelihood )
+    def solve ( self ):
+    # Place here your code to determine the maximum likelihood
+    # solution returning the solution disease name and likelihood .
+    # Use probability . elimination_ask () to perform probabilistic
+    # inference .
+        return ( disease , likelihood )
