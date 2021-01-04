4050b8347c147c2b97cd51ef0a1128bf84e74f18
Spamium <hugo00pereira@gmail.com>
2021-01-04 13:24:27 +0000

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,12 +1,33 @@
 import probability
 
 class MDProblem :
+    
     def __init__ (self , fh):
-        """
-                Initialize the network; read and parse the given file.
-                Args:
-                    fh:  Name of the file containing the data.
-                """
+        
+        # diseases will be the nodes
+        self.diseases = []
+        
+        
+        
+        dataFile = open(fh, 'r')
+        
+        for line in dataFile:
+    
+            # split input string by spaces
+            rawData = line.split(" ")
+    
+            # last string will end in /n, remove it here
+            rawData[-1] = rawData[-1][:-1]
+            
+            # last project had malformed inputs, fix lowercases here
+            rawData[0] = rawData[0].upper()
+            
+            # read the disease list and 
+            if(rawData[0] == 'D'):
+                for disease in rawData[1:]:
+                    self.diseases.append(disease)
+                
+        
         return ()
 
     def solve ( self ):
